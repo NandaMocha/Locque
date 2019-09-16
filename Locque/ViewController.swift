af4a826e7eab7fb8d1e335e6398493962f9d9349
nandamocha <nanda.mocha@gmail.com>
2019-09-16 17:31:16 +0700

    Initial Commit

---
+++ Locque/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locque
+//
+//  Created by Nanda Mochammad on 16/09/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
