2d909859335effefc1b8337c23cf940ab1c5d148
nandamocha <nanda.mocha@gmail.com>
2019-09-19 03:11:12 +0700

    add geofencing tapi gagal

--- Locque/Controller/EnterPINVC.swift
+++ Locque/Controller/EnterPINVC.swift
@@ -8,24 +8,79 @@
 
 import UIKit
 import LocalAuthentication
+import CoreLocation
 
-class EnterPINVC: UIViewController {
+class EnterPINVC: UIViewController, CLLocationManagerDelegate {
 
     let shared = DataManager.shared
+    let locationManager = CLLocationManager()
 
     @IBOutlet weak var textfieldPIN: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // 1
+        locationManager.delegate = self
+        // 2
+        locationManager.requestAlwaysAuthorization()
+        // 3
+//        loadAllGeotifications()
     }
+    
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+////        mapView.showsUserLocation = (status == .authorizedAlways)
+//    }
+//    
+//    func region(with geotification: Geotification) -> CLCircularRegion {
+//        // 1
+//        let region = CLCircularRegion(center: geotification.coordinate,
+//                                      radius: geotification.radius,
+//                                      identifier: geotification.identifier)
+//        // 2
+//        region.notifyOnEntry = (geotification.eventType == .onEntry)
+//        region.notifyOnExit = !region.notifyOnEntry
+//        return region
+//    }
+//    
+//    func startMonitoring(geotification: Geotification) {
+//        // 1
+//        if !CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+//            showAlert(withTitle:"Error", message: "Geofencing is not supported on this device!")
+//            return
+//        }
+//        // 2
+//        if CLLocationManager.authorizationStatus() != .authorizedAlways {
+//            let message = """
+//      Your geotification is saved but will only be activated once you grant
+//      Geotify permission to access the device location.
+//      """
+//            showAlert(withTitle:"Warning", message: message)
+//        }
+//        // 3
+//        let fenceRegion = region(with: geotification)
+//        // 4
+//        locationManager.startMonitoring(for: fenceRegion)
+//    }
+
+
+    
+    
+
     
     override func viewWillAppear(_ animated: Bool) {
         print("Enter PIN")
-        if shared.isUsingBiometric == true{
-            checkBiometric()
+        if checkGeofencing() == true{
+            if shared.isUsingBiometric == true{
+                checkBiometric()
+            }
+        }else{
+            shared.setAlert(title: "Attention", message: "Make sure you are around the Apple Academy", sender: self) { (done) in}
         }
+        
+    }
+    
+    func checkGeofencing()->Bool{
+        return true
     }
     
     func checkBiometric() {
@@ -36,12 +91,8 @@ class EnterPINVC: UIViewController {
                 
                 DispatchQueue.main.async {
                     self.textfieldPIN.text = self.shared.PINUser
-//                    if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainPageVC") as? MainPageVC {
-                        self.performSegue(withIdentifier: "goToMainPage", sender: self)
-//                        self.window?.rootViewController = initialViewController
-//                        self.window?.makeKeyAndVisible()
-                        
-//                    }
+                    self.performSegue(withIdentifier: "goToMainPage", sender: self)
+
                 }
             }
         }
